1e963d6fff9c53fa2b297eea8d4afc470a89e9ce
Jason Coleman <jason@strangerstudios.com>
2011-11-05 20:18:56 -0400

    Basically pulled code from http://wordpress.org/extend/plugins/disable-visual-editor-wysiwyg/ and just changed which hook is used to determine we're on the edit post page and how the javascript works a bit. Personal preference really.
    Next up is to move the disable checkbox into the screen options area using this for reference: http://w-shadow.com/blog/2010/06/29/adding-stuff-to-wordpress-screen-options/
    
    Then we'll possibly add some role stuff in here to only allow admins to check/uncheck the disable visual editor checkbox.
    
    Also going to see if I can pull in some of the stuff from this plugin to add extra protections to keep WP from filtering the content of these posts:
    
    It might actually make sense to add more option RE what kind of filters you do and don't want to run on this content (e.g. strip javascript, but don't autop)... and maybe we'll move the options back into a meta box.
    
    Heading to National Mechanics, will work more tonight.

--- dont-break-the-code.php
+++ dont-break-the-code.php
@@ -8,7 +8,99 @@ Author: Stranger Studios (WordCamp Philly)
 Author URI: http://www.strangerstudios.com
 */
 
+function dbtc_load_post()
+{
+	$cookie_path = str_replace('/wp-admin/post.php','/',$_SERVER['PHP_SELF']);
+	setcookie('wp-settings-1','m5=3&editor=html&m9=o&m1=o',0,$cookie_path);
+		
+	add_filter("format_to_edit", "dbtc_format_to_edit");
+}
+add_action("load-post.php", "dbtc_load_post");
+
+function dbtc_format_to_edit($content)
+{
+	if(isset($_GET['post']) && get_post_meta($_GET['post'], 'dbtc_checkbox') != false){
+		$cookie_path = str_replace('/wp-admin/post.php','/',$_SERVER['PHP_SELF']);
+		setcookie('wp-settings-1','m5=3&editor=html&m9=o&m1=o',0,$cookie_path);
+		
+		add_filter('admin_footer', 'dbtc_admin_footer');
+	}
+	
+	return $content;
+}
+
+function dbtc_admin_footer()
+{
+?>
+	<script type="text/javascript">
+		jQuery(document).ready(function(){
+			switchEditors.go('content', 'html');
+			jQuery('#edButtonPreview').attr('onclick', 'none');
+			var edButtonPreviewHTML = jQuery('#edButtonPreview').html();
+			jQuery('#edButtonPreview').html('<span style="text-decoration:line-through">'+edButtonPreviewHTML+'</span>');
+		});
+	</script>
+<?php
+}
+
+function dbtc_add_custom_box() {
+    add_meta_box( 
+        'dbtc_sectionid',
+        __( 'Visual Editor', 'dbtc_textbox' ),
+        'dbtc_custom_box',
+        'post',
+		'side',
+		'default'
+    );
+    add_meta_box(
+        'dbtc_sectionid',
+        __( 'Visual Editor', 'dbtc_textbox' ), 
+        'dbtc_custom_box',
+        'page',
+		'side',
+		'default'
+    );
+}
 
 
+function dbtc_custom_box() {
+
+  wp_nonce_field( plugin_basename( __FILE__ ), 'dbtc_noncename' );
+
+  $checked = "";
+  if(isset($_GET['post']) && get_post_meta($_GET['post'], 'dbtc_checkbox') != false) $checked = ' checked="checked" ';
+
+  echo '<input type="checkbox" id="dbtc_checkbox" name="dbtc_checkbox" '.$checked.'/>';
+  echo '<label for="dbtc_checkbox">';
+       _e(" Disable", 'dbtc_textbox' );
+  echo '</label> ';
+}
+add_action( 'admin_init', 'dbtc_add_custom_box', 1 );
+
+function dbtc_save_post( $post_id ) {
+  if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) 
+      return;
+
+  if ( !wp_verify_nonce( $_POST['dbtc_noncename'], plugin_basename( __FILE__ ) ) )
+      return;
+
+  if ( 'page' == $_POST['post_type'] ) 
+  {
+    if ( !current_user_can( 'edit_page', $post_id ) )
+        return;
+  }
+  else
+  {
+    if ( !current_user_can( 'edit_post', $post_id ) )
+        return;
+  }
+
+  if(isset($_POST['dbtc_checkbox'])){
+	  if(!get_post_meta($post_id, 'dbtc_checkbox')) add_post_meta($post_id, 'dbtc_checkbox', 1);
+  }else{
+	  delete_post_meta($post_id, 'dbtc_checkbox');
+  }
+}
+add_action( 'save_post', 'dbtc_save_post' );
 ?>
 
